7e7c55617634210fa3c9bd498fe54aeaa078dfab
chetan kuamr <chetankumarpulipati4@gmail.com>
2024-06-09 09:43:43 +0530

    login screen

--- app/src/main/java/com/gdsc/gdsc_mbu/ShareIdea.kt
+++ app/src/main/java/com/gdsc/gdsc_mbu/ShareIdea.kt
@@ -1,13 +1,29 @@
 package com.gdsc.gdsc_mbu
 
-import androidx.compose.material.Divider
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.gdsc.gdsc_mbu.ui.theme.lightblack
 
 @Composable
 fun ideaspot(){
-    Divider(thickness=2.5.dp, color= lightblack)
+    CustomDivider(color = lightblack, thickness = 2.5.dp)
     Text("Ideaspot")
+}
+
+@Composable
+fun CustomDivider(color: Color, thickness: Dp) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(thickness)
+            .background(color)
+    )
 }
